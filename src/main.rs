ef07a9924bb19a49d44d2e1013bc85ac3ee603d3
Leo Mansini <leomansini2000@gmail.com>
2024-07-23 03:08:58 -0300

    fix accion

--- src/main.rs
+++ src/main.rs
@@ -30,42 +30,63 @@ fn puedo_ir_push_a_pop(s: &mut Stack<usize>) {
 
 fn puedo_ir_pop_a_pop(s: &mut Stack<usize>) {
     if !s.req_push() && s.req_pop() {
-        s.push(1);
+        s.pop();
         assert!(!s.req_push() && s.req_pop())
     }
 }
 
 fn puedo_ir_pop_a_push(s: &mut Stack<usize>) {
     if !s.req_push() && s.req_pop() {
-        s.push(1);
+        s.pop();
         assert!(s.req_push() && !s.req_pop())
     }
 }
 
 fn puedo_ir_pop_a_pushpop(s: &mut Stack<usize>) {
     if !s.req_push() && s.req_pop() {
-        s.push(1);
+        s.pop();
         assert!(s.req_push() && s.req_pop())
     }
 }
 
-fn puedo_ir_pushpop_a_push(s: &mut Stack<usize>) {
+fn puedo_ir_pushpop_a_push_con_push(s: &mut Stack<usize>) {
     if s.req_push() && s.req_pop() {
         s.push(1);
         assert!(s.req_push() && !s.req_pop())
     }
 }
 
-fn puedo_ir_pushpop_a_pop(s: &mut Stack<usize>) {
+fn puedo_ir_pushpop_a_pop_con_push(s: &mut Stack<usize>) {
     if s.req_push() && s.req_pop() {
         s.push(1);
         assert!(!s.req_push() && s.req_pop())
     }
 }
 
-fn puedo_ir_pushpop_a_pushpop(s: &mut Stack<usize>) {
+fn puedo_ir_pushpop_a_pushpop_con_push(s: &mut Stack<usize>) {
     if s.req_push() && s.req_pop() {
         s.push(1);
+        assert!(s.req_push() && s.req_pop())
+    }
+}
+
+fn puedo_ir_pushpop_a_push_con_pop(s: &mut Stack<usize>) {
+    if s.req_push() && s.req_pop() {
+        s.pop();
+        assert!(s.req_push() && !s.req_pop())
+    }
+}
+
+fn puedo_ir_pushpop_a_pop_con_pop(s: &mut Stack<usize>) {
+    if s.req_push() && s.req_pop() {
+        s.pop();
+        assert!(!s.req_push() && s.req_pop())
+    }
+}
+
+fn puedo_ir_pushpop_a_pushpop_con_pop(s: &mut Stack<usize>) {
+    if s.req_push() && s.req_pop() {
+        s.pop();
         assert!(s.req_push() && s.req_pop())
     }
 }
