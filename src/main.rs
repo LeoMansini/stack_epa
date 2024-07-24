b9f74c4664fa4e453b51d8cd0fded52a8ffa5181
Leo Mansini <leomansini2000@gmail.com>
2024-07-24 19:52:36 -0300

    attempt to run all transition checks

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,6 @@ use transitions::puedo_ir_push_a_pushpop;
 fn main() {
     println!("Hello, world!");
     let mut s: Stack<usize> = Stack::new(2usize);
-    puedo_ir_push_a_pushpop(&mut s);
+    //puedo_ir_push_a_pushpop(&mut s);
 }
 
--- src/transitions.rs
+++ src/transitions.rs
@@ -1,85 +1,98 @@
 use crate::stack::Stack;
+use crate::kani_model_checking::non_deterministic_stack;
 
-pub fn puedo_ir_push_a_pushpop(s: &mut Stack<usize>) {
-    if s.req_push() && !s.req_pop() {
-        s.push(1);
-        assert!(s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_push_a_pushpop() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && !s.req_pop());
+    s.push(1);
+    assert!(s.req_push() && s.req_pop())
 }
 
-pub fn puedo_ir_push_a_push(s: &mut Stack<usize>) {
-    if s.req_push() && !s.req_pop() {
-        s.push(1);
-        assert!(s.req_push() && !s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_push_a_push() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && !s.req_pop());
+    s.push(1);
+    assert!(s.req_push() && !s.req_pop())
 }
 
-pub fn puedo_ir_push_a_pop(s: &mut Stack<usize>) {
-    if s.req_push() && !s.req_pop() {
-        s.push(1);
-        assert!(!s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_push_a_pop() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && !s.req_pop());
+    s.push(1);
+    assert!(!s.req_push() && s.req_pop())
 }
 
-pub fn puedo_ir_pop_a_pop(s: &mut Stack<usize>) {
-    if !s.req_push() && s.req_pop() {
-        s.pop();
-        assert!(!s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pop_a_pop() {
+    let mut s = non_deterministic_stack();
+    kani::assume(!s.req_push() && s.req_pop());
+    s.pop();
+    assert!(!s.req_push() && s.req_pop())
 }
 
-pub fn puedo_ir_pop_a_push(s: &mut Stack<usize>) {
-    if !s.req_push() && s.req_pop() {
-        s.pop();
-        assert!(s.req_push() && !s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pop_a_push() {
+    let mut s = non_deterministic_stack();
+    kani::assume(!s.req_push() && s.req_pop());
+    s.pop();
+    assert!(s.req_push() && !s.req_pop())
 }
 
-pub fn puedo_ir_pop_a_pushpop(s: &mut Stack<usize>) {
-    if !s.req_push() && s.req_pop() {
-        s.pop();
-        assert!(s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pop_a_pushpop() {
+    let mut s = non_deterministic_stack();
+    kani::assume(!s.req_push() && s.req_pop());
+    s.pop();
+    assert!(s.req_push() && s.req_pop())
 }
 
-pub fn puedo_ir_pushpop_a_push_con_push(s: &mut Stack<usize>) {
-    if s.req_push() && s.req_pop() {
-        s.push(1);
-        assert!(s.req_push() && !s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pushpop_a_push_con_push() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && s.req_pop());
+    s.push(1);
+    assert!(s.req_push() && !s.req_pop())
 }
 
-pub fn puedo_ir_pushpop_a_pop_con_push(s: &mut Stack<usize>) {
-    if s.req_push() && s.req_pop() {
-        s.push(1);
-        assert!(!s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pushpop_a_pop_con_push() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && s.req_pop());
+    s.push(1);
+    assert!(!s.req_push() && s.req_pop())
 }
 
-pub fn puedo_ir_pushpop_a_pushpop_con_push(s: &mut Stack<usize>) {
-    if s.req_push() && s.req_pop() {
-        s.push(1);
-        assert!(s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pushpop_a_pushpop_con_push() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && s.req_pop());
+    s.push(1);
+    assert!(s.req_push() && s.req_pop())
 }
 
-pub fn puedo_ir_pushpop_a_push_con_pop(s: &mut Stack<usize>) {
-    if s.req_push() && s.req_pop() {
-        s.pop();
-        assert!(s.req_push() && !s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pushpop_a_push_con_pop() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && s.req_pop());
+    s.pop();
+    assert!(s.req_push() && !s.req_pop())
 }
 
-pub fn puedo_ir_pushpop_a_pop_con_pop(s: &mut Stack<usize>) {
-    if s.req_push() && s.req_pop() {
-        s.pop();
-        assert!(!s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pushpop_a_pop_con_pop() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && s.req_pop());
+    s.pop();
+    assert!(!s.req_push() && s.req_pop())
 }
 
-pub fn puedo_ir_pushpop_a_pushpop_con_pop(s: &mut Stack<usize>) {
-    if s.req_push() && s.req_pop() {
-        s.pop();
-        assert!(s.req_push() && s.req_pop())
-    }
+#[cfg(kani)]
+pub fn puedo_ir_pushpop_a_pushpop_con_pop() {
+    let mut s = non_deterministic_stack();
+    kani::assume(s.req_push() && s.req_pop());
+    s.pop();
+    assert!(s.req_push() && s.req_pop())
 }
