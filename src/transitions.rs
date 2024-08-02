f75e48fda3afc76faf044b91974f0875e7595c85
Leo Mansini <leomansini2000@gmail.com>
2024-08-02 02:31:34 -0300

    fixed, finally

--- src/transitions.rs
+++ src/transitions.rs
@@ -25,110 +25,126 @@ pub fn non_deterministic_stack() -> Stack<usize> {
 
 }
 
+pub fn estoy_en_pop(s: &Stack<usize>) -> bool {
+    !s.req_push() && s.req_pop()
+}
+
+pub fn estoy_en_push(s: &Stack<usize>) -> bool {
+    s.req_push() && !s.req_pop()
+}
+
+pub fn estoy_en_pushpop(s: &Stack<usize>) -> bool {
+    s.req_push() && s.req_pop()
+}
+
+pub fn estoy_en_nada(s: &Stack<usize>) -> bool {
+    !s.req_push() && !s.req_pop()
+}
+
 
 #[kani::proof]
 pub fn puedo_ir_push_a_pushpop() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && !s.req_pop());
+    kani::assume(estoy_en_push(&s));
     s.push(1);
-    assert!(s.req_push() && s.req_pop())
+    assert!(!estoy_en_pushpop(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_push_a_push() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && !s.req_pop());
+    kani::assume(estoy_en_push(&s));
     s.push(1);
-    assert!(s.req_push() && !s.req_pop())
+    assert!(!estoy_en_push(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_push_a_pop() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && !s.req_pop());
+    kani::assume(estoy_en_push(&s));
     s.push(1);
-    assert!(!s.req_push() && s.req_pop())
+    assert!(!estoy_en_pop(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pop_a_pop() {
     let mut s = non_deterministic_stack();
-    kani::assume(!s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pop(&s));
     s.pop();
-    assert!(!s.req_push() && s.req_pop())
+    assert!(!estoy_en_pop(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pop_a_push() {
     let mut s = non_deterministic_stack();
-    kani::assume(!s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pop(&s));
     s.pop();
-    assert!(s.req_push() && !s.req_pop())
+    assert!(!estoy_en_push(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pop_a_pushpop() {
     let mut s = non_deterministic_stack();
-    kani::assume(!s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pop(&s));
     s.pop();
-    assert!(s.req_push() && s.req_pop())
+    assert!(!estoy_en_pushpop(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pushpop_a_push_con_push() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pushpop(&s));
     s.push(1);
-    assert!(s.req_push() && !s.req_pop())
+    assert!(!estoy_en_push(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pushpop_a_pop_con_push() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pushpop(&s));
     s.push(1);
-    assert!(!s.req_push() && s.req_pop())
+    assert!(!estoy_en_pop(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pushpop_a_pushpop_con_push() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pushpop(&s));
     s.push(1);
-    assert!(s.req_push() && s.req_pop())
+    assert!(!estoy_en_pushpop(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pushpop_a_push_con_pop() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pushpop(&s));
     s.pop();
-    assert!(s.req_push() && !s.req_pop())
+    assert!(!estoy_en_push(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pushpop_a_pop_con_pop() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pushpop(&s));
     s.pop();
-    assert!(!s.req_push() && s.req_pop())
+    assert!(!estoy_en_pop(&s))
 }
 
 
 #[kani::proof]
 pub fn puedo_ir_pushpop_a_pushpop_con_pop() {
     let mut s = non_deterministic_stack();
-    kani::assume(s.req_push() && s.req_pop());
+    kani::assume(estoy_en_pushpop(&s));
     s.pop();
-    assert!(s.req_push() && s.req_pop())
+    assert!(!estoy_en_pushpop(&s))
 }
