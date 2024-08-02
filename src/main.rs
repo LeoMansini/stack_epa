cc310fee21682db039bc9f3a6627a45eeb6f217c
Leo Mansini <leomansini2000@gmail.com>
2024-08-02 01:05:57 -0300

    settings to debug and lint

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ use stack::Stack;
 mod transitions;
 
 fn main() {
+    {
+        let url = format!(
+            "vscode://vadimcn.vscode-lldb/launch/config?{{'request':'attach','sourceLanguages':['rust'],'waitFor':true,'pid':{}}}",
+            std::process::id()
+        );
+        std::process::Command::new("code").arg("--open-url").arg(url).output().unwrap();
+    }
     println!("Hello, world!");
 }
 
